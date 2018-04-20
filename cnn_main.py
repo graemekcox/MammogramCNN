786d1139802021d50ca6348326eb85adabff1936
gramekcox <graeme.k.cox@gmail.com>
2018-04-20 18:25:40 -0400

    Cleaned up main by replacing some variables with Tensorflow Flags

--- cnn_main.py
+++ cnn_main.py
@@ -15,16 +15,23 @@ import numpy as np
 from PIL import Image
 import os
 
+
+## Set flags for 
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_boolean('use_ex_data',True,
+	"""Choose whether to use MNIST dataset instead of DDSM""")
+tf.app.flags.DEFINE_string('data_dir','//Volumes/ExternalDrive/Mammogram',
+	"""Set the root folder containing Bening, Cancer and Normal cases""")
+tf.app.flags.DEFINE_boolean('prep_images',False,
+	"""Sets whether to pre-process dataset. Otherwise load last saved numpy array""")
+
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Ignore CPU instructions warning since we're using GPU
 
-##Parameters to set before running
-USE_EX_DATA = 0
-PREP_IMAGES = 0
-# root = '/Users/graemecox/Documents/ResearchProject/Data/Mammograms'
-DATA_DIR =  '//Volumes/ExternalDrive/Mammograms'
 
 ## Use the mnist dataset if you want to see how the program works. Much faster 
-if (USE_EX_DATA):
+if (FLAGS.use_ex_data):
 	IMAGE_SIZE=28
 	NUM_CLASSES = 10
 	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -38,16 +45,8 @@ else:
 #Learning variables
 LR = 0.0001
 epochs = 10
-batch_size = 10
+batch_size = 100
 
-
-def next_batch(num, data, labels):
-	idx = np.arange(0, len(data))
-	np.random.shuffle(idx)
-	idx = idx[:num]
-	data_shuffle = [data[i] for i in idx]
-	labels_shuffle = [labels[i] for i in idx]
-	return np.asarray(data_shuffle), np.asarray(labels_shuffle)
 
 def main():
 	x = tf.placeholder(tf.float32,[None,IMAGE_SIZE*IMAGE_SIZE])
@@ -93,40 +92,26 @@ def main():
 
 
 ################################## GET IMAGES READY ######################################
-	# images, labels = prepImages(root, IMAGE_SIZE, IMAGE_SIZE,1)
 
-	# images = np.load('/Users/graemecox/Documents/ResearchProject/Code/Data/1000_1000/image_1000_1000.npy')
-	if not USE_EX_DATA:
-		# if PREP_IMAGES:
-		# 	images,labels = prepImages(DATA_DIR, IMAGE_SIZE, IMAGE_SIZE, 1)
-		# images = np.load('/Users/graemecox/Documents/ResearchProject/Data/500_500/image_b1.npy')
-		# labels = np.load('/Users/graemecox/Documents/ResearchProject/Data/500_500/labels_b1.npy')
+	## Use DDSM Dataset
+	if not FLAGS.use_ex_data:
 
-		# train_images, test_images, train_labels, test_labels = train_test_split(
-		# 	images, labels, test_size=0.2, random_state=0)
-
-		# print('Length of training images: ' + str(len(train_images)))
-		# print('Length of training labels: ' + str(len(train_labels)))
-
-		# print(train_labels.shape)
-		# print(test_labels.shape)
-		# print(train_images.shape)
-		# print(test_images.shape)
 		ddsm= imagePrep.prepDDSM(PREP_IMAGES)
 		train_labels = ddsm.train_labels
 		train_images = ddsm.train_images
 		test_labels  = ddsm.test_labels
 		test_images  = ddsm.test_images
-		print(ddsm)
 
 
-	if USE_EX_DATA:
+	## Use MNIST Dataset
+	else:
 		train_labels = mnist.train.labels
 		train_images = mnist.train.images
 		test_labels = mnist.test.labels
 		test_images = mnist.test.images
 ##############################################################################################
 
+	## Only use CPU
 	config = tf.ConfigProto(
 		device_count = {'GPU':0}
 		)
@@ -139,20 +124,18 @@ def main():
 
 		for epoch in range(epochs):
 			avg_cost = 0
+
+			# Run through multiple different batches
 			for i in range(total_batch):
-				if USE_EX_DATA:
+				if FLAGS.use_ex_data:
 					batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
-					# print(batch_x.shape)
-					# print(batch_y.shape)
 				else:
-					batch_x, batch_y = next_batch(batch_size, train_images, train_labels)
-					# print(batch_x.shape)
-					# print(batch_y.shape)
+					batch_x, batch_y = imagePrep.next_batch(batch_size, train_images, train_labels)
 
 				_, c = sess.run([optimiser, cross_entropy],
 					feed_dict={x:batch_x, y:batch_y})
 				avg_cost += c/total_batch
-			if (USE_EX_DATA):
+			if (FLAGS.use_ex_data):
 				test_acc = sess.run(accuracy,
 					feed_dict={x:mnist.test.images, y:mnist.test.labels})
 
@@ -164,8 +147,8 @@ def main():
 				"test accuracy: {:.3f}".format(test_acc))
 		print("\nTraining complete!")
 
-
-		if (USE_EX_DATA):
+		# Use test data to determine accuracies
+		if (FLAGS.use_ex_data):
 			print(sess.run(accuracy,feed_dict={x:mnist.test.images, y:mnist.test.labels}))
 		else:
 			print(sess.run(accuracy,feed_dict={x:test_images, y:test_labels}))
