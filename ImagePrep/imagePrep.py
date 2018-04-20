a15c3e279e13aebba9fc49a9e0b7c8a42c4f6633
gramekcox <graeme.k.cox@gmail.com>
2018-04-20 18:26:21 -0400

    Added 'next_batch' function

--- ImagePrep/imagePrep.py
+++ ImagePrep/imagePrep.py
@@ -168,7 +168,16 @@ def prepDDSM(PREP_IMAGES, max_x=500, max_y=500):
 	return output
 
 
+def next_batch(num, data, labels):
+	idx = np.arange(0, len(data))
+	np.random.shuffle(idx)
+	idx = idx[:num]
+	data_shuffle = [data[i] for i in idx]
+	labels_shuffle = [labels[i] for i in idx]
+	return np.asarray(data_shuffle), np.asarray(labels_shuffle)
 
+
+	
 ### Uncomment to prep images
 # root = '/Volumes/ExternalDrive/Mammograms/'
 # max_x = 500
